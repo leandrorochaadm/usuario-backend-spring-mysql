eaba27e11739aaf10d7d57d7edb124ae69bc281d
leandrorochaadm <leandrorochaadm@gmail.com>
2020-11-05 22:05:48 -0400

    criacao do projeto

---
+++ src/main/java/com/leandrorocha/wkusuariosbackend/WkUsuariosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leandrorocha.wkusuariosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WkUsuariosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WkUsuariosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leandrorocha/wkusuariosbackend/WkUsuariosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandrorocha.wkusuariosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WkUsuariosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
